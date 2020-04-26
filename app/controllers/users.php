7975c436aa1ad97c06a97f04e11ded8d598ad946
uthmanigbin <uthmanigbin@gmail.com>
2020-04-26 16:29:03 +0300

    reusable functions

---
+++ app/controllers/users.php
@@ -0,0 +1,6 @@
+<?php 
+if (isset($_POST['register-btn'])) {
+  var_dump($_POST);
+  die();
+}
+?>
--- app/database/db.php
+++ app/database/db.php
@@ -8,6 +8,17 @@ function dd($value) // to be deleted
 	die();
 }
 
+function executeQuery($sql, $data)
+{
+	global $conn;
+	$stmt = $conn->prepare($sql);
+	$values = array_values($data);
+	$types = str_repeat('s', count($values));
+	$stmt->bind_param($types, ...$values);
+	$stmt->execute();
+	return $stmt;
+}
+
 function selectAll($table, $conditions = [])
 {
 	global $conn;
@@ -28,11 +39,7 @@ function selectAll($table, $conditions = [])
 			$i++;
 		}
 
-		$stmt = $conn->prepare($sql);
-		$values = array_values($conditions);
-		$types = str_repeat('s', count($values));
-		$stmt->bind_param($types, ...$values);
-		$stmt->execute();
+		$stmt = executeQuery($sql, $conditions);
 		$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 		return $records;
 	}
@@ -43,38 +50,75 @@ function selectOne($table, $conditions)
 {
 	global $conn;
 	$sql = "SELECT * FROM $table";
-	if (empty($conditions)) {
-		$stmt = $conn->prepare($sql);
-		$stmt->execute();
-		$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-		return $records;
-	} else {
-		$i = 0;
-		foreach ($conditions as $key => $value) {
-			if ($i === 0) {
-				$sql = $sql . " WHERE $key=?";
-			} else {
-				$sql = $sql . " AND $key=?";
-			}
-			$i++;
+	$i = 0;
+	foreach ($conditions as $key => $value) {
+		if ($i === 0) {
+			$sql = $sql . " WHERE $key=?";
+		} else {
+			$sql = $sql . " AND $key=?";
 		}
-
-		$stmt = $conn->prepare($sql);
-		$values = array_values($conditions);
-		$types = str_repeat('s', count($values));
-		$stmt->bind_param($types, ...$values);
-		$stmt->execute();
-		$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-		return $records;
+		$i++;
 	}
+
+	$sql = $sql . " LIMIT 1";
+	$stmt = executeQuery($sql, $conditions);
+	$records = $stmt->get_result()->fetch_assoc();
+	return $records;
 }
 
-$conditions = [
-	'admin' => 0,
-	'username' => "Uthmanigbin"
-];
+function create($table, $data)
+{
+	global $conn;
+	//$sql = "INSERT INTO $table SET username=?, admin=?, email=?, password=?"
+	$sql = "INSERT INTO $table SET ";
+	$i = 0;
+	foreach ($data as $key => $value) {
+		if ($i === 0) {
+			$sql = $sql . " $key=?";
+		} else {
+			$sql = $sql . ", $key=?";
+		}
+		$i++;
+	}
 
-$users = selectAll('users', $conditions);
-dd($users);
+	$stmt = executeQuery($sql, $data);
+	$id = $stmt->insert_id;
+	return $id;
+}
+
+function update($table, $id, $data)
+{
+	global $conn;
+	//$sql = "UPDATE $table SET username=?, admin=?, email=?, password=? WHERE id=?"
+	$sql = "UPDATE $table SET ";
+	$i = 0;
+	foreach ($data as $key => $value) {
+		if ($i === 0) {
+			$sql = $sql . " $key=?";
+		} else {
+			$sql = $sql . ", $key=?";
+		}
+		$i++;
+	}
+
+	$sql = $sql . " WHERE id=?";
+	$data['id'] = $id;
+	$stmt = executeQuery($sql, $data);
+	return $stmt->affected_rows;
+}
+
+function delete($table, $id)
+{
+	global $conn;
+	$sql = "DELETE FROM $table WHERE id=?";
+
+	$stmt = executeQuery($sql, ['id' => $id]);
+	return $stmt->affected_rows;
+}
+
+
+
+$id = delete('users', 2);
+dd($id);
 
 ?>
